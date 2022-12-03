2cb8d06e5373505a8f5c2567ccc2601007747316
Cheryl Danner <sheromon@users.noreply.github.com>
2022-12-03 00:32:09 -0500

    Spiffed up day 2 a little bit.

--- day02/day02.py
+++ day02/day02.py
@@ -7,15 +7,18 @@ def day02a(input_path):
         for line in file_obj:
             score = 0
             moves = [ord(move) for move in line.strip().split()]
-            moves[0] -= 64  # A --> 1, B --> 2, C --> 3
-            moves[1] -= 87  # X --> 1, Y --> 2, Z --> 3
+            yours, mine = moves
+            yours -= 64  # A --> 1, B --> 2, C --> 3
+            mine -= 87  # X --> 1, Y --> 2, Z --> 3
             # A means opponent will play rock, Y paper, Z scissors
             # X means I should play rock, Y paper, Z scissors
-            score += moves[1]  # shape points
-            outcome_points = 0
-            if moves[0] == moves[1]:
+            score += mine  # shape points
+            offset = (yours - mine) % 3
+            if offset == 0:
                 outcome_points = 3  # draw
-            elif (moves[0] == moves[1] - 1) or (moves[0] == moves[1] + 2):
+            elif offset == 1:
+                outcome_points = 0  # lose
+            else:
                 outcome_points = 6  # win
             score += outcome_points
             total += score
@@ -31,27 +34,26 @@ def day02b(input_path):
     with open(input_path) as file_obj:
         total = 0
         for line in file_obj:
-            score = 0
             moves = [ord(move) for move in line.strip().split()]
-            moves[0] -= 64  # A --> 1, B --> 2, C --> 3
-            moves[1] -= 87  # X --> 1, Y --> 2, Z --> 3
+            yours, outcome = moves
+            yours -= 64  # A --> 1, B --> 2, C --> 3
+            outcome -= 87  # X --> 1, Y --> 2, Z --> 3
             # A means opponent will play rock, Y paper, Z scissors
             # X means I should lose, Y means I should draw, Z means I should win
-            if moves[1] == 1:  # lose
+            if outcome == 1:  # lose
                 outcome_points = 0
-                shape_points = moves[0] - 1
+                shape_points = yours - 1
                 if shape_points < 1:
                     shape_points += 3
-            elif moves[1] == 2:  # draw
+            elif outcome == 2:  # draw
                 outcome_points = 3
-                shape_points = moves[0]
+                shape_points = yours
             else:  # win
                 outcome_points = 6
-                shape_points = moves[0] + 1
+                shape_points = yours + 1
                 if shape_points > 3:
                     shape_points -= 3
-            score += shape_points
-            score += outcome_points
+            score = shape_points + outcome_points
             total += score
     return total
 
