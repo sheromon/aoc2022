9d5416b54d07d4021e57d4be48e7574494ab1118
Cheryl Danner <sheromon@users.noreply.github.com>
2023-01-03 20:02:50 -0500

    Added code for day 16, part 2. It's slow, but it will have to do for now.

--- day16/day16.py
+++ day16/day16.py
@@ -6,8 +6,7 @@ def day16a(input_path):
     # we only care about opening valves that have a nonzero flow rate
     valve_name_list = [name for name, valve in valves.items() if valve.rate > 0]
 
-    # pre-calculate min distances from each valve of interest (plus starting valve) to all other valves
-    distance_map = DistanceMap(valves, valve_name_list + ['AA'])
+    evaluator = Evaluator(valves, valve_name_list)
 
     total_pressure_list = []
     # the total pressure released is a function of which valves are opened in what order, so we will calculate the total
@@ -15,11 +14,10 @@ def day16a(input_path):
     next_sequences = [[name] for name in valve_name_list]
     while next_sequences:
         sequences = next_sequences
-        # next round of sequences will be sequences that could be completed this round (with time to open another valve)
-        # with one additional valve
+        # next round of sequences will be sequences that could be completed this round plus one additional valve
         next_sequences = []
         for sequence in sequences:
-            completed, total = evaluate_valve_sequence(valves, distance_map, sequence)
+            completed, total = evaluator.evaluate_valve_sequence(sequence)
             if completed:
                 next_valve_choices = set(valve_name_list) - set(sequence)
                 next_sequences += [sequence + [next_valve] for next_valve in next_valve_choices]
@@ -62,45 +60,50 @@ class DistanceMap:
     def __init__(self, valves, valve_name_list):
         self.from_map = dict()
         for valve_name in valve_name_list:
-            self.from_map[valve_name] = calc_distances(valves, valve_name)
+            self._calc_distances(valves, valve_name)
 
     def get_min_distance(self, from_name, to_name):
         """Return the minimum number of steps between two valves."""
         return self.from_map[from_name][to_name]
 
+    def _calc_distances(self, valves, from_valve):
+        """Calculate the minimum distance from one valve to all other valves and return the results as a dictionary."""
 
-def calc_distances(valves, from_valve):
-    """Calculate the minimum distance from one valve to all other valves and return the results as a dictionary."""
+        def recursive_distance(dist_map, valve, min_dist):
+            for name in valve.connections:
+                if name not in dist_map or min_dist < dist_map[name]:
+                    dist_map[name] = min_dist
+                    recursive_distance(dist_map, valves[name], min_dist + 1)
 
-    def recursive_distance(dist_map, valve, min_dist):
-        for name in valve.connections:
-            if name not in dist_map or min_dist < dist_map[name]:
-                dist_map[name] = min_dist
-                recursive_distance(dist_map, valves[name], min_dist + 1)
-
-    min_distance_map = {from_valve: 0}
-    recursive_distance(min_distance_map, valves[from_valve], 1)
-    return min_distance_map
+        self.from_map[from_valve] = {from_valve: 0}
+        recursive_distance(self.from_map[from_valve], valves[from_valve], 1)
 
 
-def evaluate_valve_sequence(valves, distance_map, sequence):
-    """Determine whether a sequence of valves can all be opened within the time limit and the pressure released."""
-    current_valve_name = 'AA'
-    total = 0
-    minutes_remaining = 30
-    completed = True
-    for ind, valve_name in enumerate(sequence):
-        steps = distance_map.get_min_distance(current_valve_name, valve_name)
-        if steps > minutes_remaining:
-            completed = False
-            break
-        minutes_remaining -= steps + 1
-        total += minutes_remaining * valves[valve_name].rate
-        current_valve_name = valve_name
-    # what we really care about is if we had enough time to open all valves in this sequence and then open another valve
-    # after that, so add a check on time remaining
-    completed = completed and minutes_remaining > 2
-    return completed, total
+class Evaluator:
+
+    def __init__(self, valves, nonzero_valve_names):
+        self.valves = valves
+        # pre-calculate min distances from each valve of interest (plus starting valve) to all other valves
+        self.distance_map = DistanceMap(valves, nonzero_valve_names + ['AA'])
+
+    def evaluate_valve_sequence(self, sequence, max_time=30):
+        """Determine whether a sequence of valves can all be opened within the time limit and the pressure released."""
+        current_valve_name = 'AA'
+        total = 0
+        minutes_remaining = max_time
+        completed = True
+        for ind, valve_name in enumerate(sequence):
+            steps = self.distance_map.get_min_distance(current_valve_name, valve_name)
+            if steps > minutes_remaining:
+                completed = False
+                break
+            minutes_remaining -= steps + 1
+            total += minutes_remaining * self.valves[valve_name].rate
+            current_valve_name = valve_name
+        # what we really care about is if we had enough time to open all valves in this sequence and then open another
+        # valve after that, so add a check on time remaining
+        completed = completed and minutes_remaining > 2
+        return completed, total
 
 
 def test16a():
@@ -108,7 +111,49 @@ def test16a():
 
 
 def day16b(input_path):
-    pass
+    """Return the maximum units of pressure that can be released in 26 minutes by opening valves with help."""
+    valves = parse_input(input_path)
+    # we only care about opening valves that have a nonzero flow rate
+    valve_name_list = [name for name, valve in valves.items() if valve.rate > 0]
+
+    evaluator = Evaluator(valves, valve_name_list)
+
+    # this time, record all sequences checked with corresponding pressure released
+    total_pressure_map = {}
+    # the total pressure released is a function of which valves are opened in what order, so we will calculate the total
+    # pressure for various sequences of valves being opened, starting with sequences of length one
+    next_sequences = [[name] for name in valve_name_list]
+    while next_sequences:
+        sequences = next_sequences
+        # next round of sequences will be sequences that could be completed this round plus one additional valve
+        next_sequences = []
+        for sequence in sequences:
+            completed, total = evaluator.evaluate_valve_sequence(sequence, max_time=26)
+            if completed:
+                next_valve_choices = set(valve_name_list) - set(sequence)
+                next_sequences += [sequence + [next_valve] for next_valve in next_valve_choices]
+            total_pressure_map[tuple(sequence)] = total
+
+    # sort sequences from most to least pressure released
+    pressure_list = sorted(total_pressure_map.items(), key=lambda x: x[1], reverse=True)
+
+    # check pairs of sequences starting from highest total values
+    max_total_pressure = 0
+    max_ind = 2
+    while max_ind < len(pressure_list) - 1:
+        for ind in range(max_ind - 1):
+            seq0 = pressure_list[ind][0]
+            seq1 = pressure_list[max_ind][0]
+            # if the two sequences share any valves, the result is invalid
+            if set(seq0) & set(seq1):
+                continue
+            total_pressure = pressure_list[ind][1] + pressure_list[max_ind][1]
+            max_total_pressure = max(max_total_pressure, total_pressure)
+        max_ind += 1
+        # stop checking sequences when we think we've probably found the highest valid total
+        if pressure_list[max_ind][1] < max_total_pressure * 0.4:
+            break
+    return max_total_pressure
 
 
 def test16b():
@@ -118,5 +163,5 @@ def test16b():
 if __name__ == '__main__':
     test16a()
     print('Day 16a:', day16a('day16_input.txt'))
-    # test16b()
-    # print('Day 16b:', day16b('day16_input.txt'))
+    test16b()
+    print('Day 16b:', day16b('day16_input.txt'))
